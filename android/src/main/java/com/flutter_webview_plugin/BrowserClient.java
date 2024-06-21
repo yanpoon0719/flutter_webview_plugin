cc62394c2daa128ef7508e185daf30e654450b1d
YanPoon <yan@hktaxiapp.com>
2024-06-21 18:41:19 +0800

    roll back set INVISIBLE when error exist and reset back when pageStart

--- android/src/main/java/com/flutter_webview_plugin/BrowserClient.java
+++ android/src/main/java/com/flutter_webview_plugin/BrowserClient.java
@@ -63,7 +63,6 @@ public class BrowserClient extends WebViewClient {
         Log.d("onPageStarted", "shouldStartUrlHeaders: "+shouldStartUrlHeaders);
 
         data.put("type", "startLoad");
-        view.setVisibility(View.VISIBLE);
         FlutterWebviewPlugin.channel.invokeMethod("onState", data);
     }
 
@@ -142,7 +141,6 @@ public class BrowserClient extends WebViewClient {
     @Override
     public void onReceivedHttpError(WebView view, WebResourceRequest request, WebResourceResponse errorResponse) {
         super.onReceivedHttpError(view, request, errorResponse);
-        view.setVisibility(View.INVISIBLE);
         Map<String, Object> data = new HashMap<>();
         data.put("url", request.getUrl().toString());
         data.put("code", Integer.toString(errorResponse.getStatusCode()));
@@ -154,7 +152,6 @@ public class BrowserClient extends WebViewClient {
     @Override
     public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
         super.onReceivedError(view, errorCode, description, failingUrl);
-        view.setVisibility(View.INVISIBLE);
         Map<String, Object> data = new HashMap<>();
         data.put("url", failingUrl);
         data.put("code", Integer.toString(errorCode));
